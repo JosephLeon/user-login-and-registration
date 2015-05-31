4e944d81c525cf3e68667a53f59de7e4c989f5ae
Joseph Leon <joseph.leon.9@gmail.com>
2015-05-31 11:07:57 -0400

    Add RegisterPageViewController, link up text fields with the RegisterPageViewController.

---
+++ UserLoginAndRegistration/RegisterPageViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RegisterPageViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by Joseph Leon on 5/31/15.
+//  Copyright (c) 2015 Joseph Leon. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController {
+
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func registerTapped(sender: AnyObject) {
+        
+        let userEmail = userEmailTextField.text
+        let userPassword = userPasswordTextField.text
+        let repeatPassword = repeatPasswordTextField.text
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
