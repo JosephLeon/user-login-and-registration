20e6814335d197671e9f12083319e708af79af75
Joseph Leon <joseph.leon.9@gmail.com>
2015-05-31 10:28:21 -0400

    Add code to call perform segue with identifier loginView on protected view controller.

--- UserLoginAndRegistration/ViewController.swift
+++ UserLoginAndRegistration/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        self.performSegueWithIdentifier("loginView", sender: self)
+    }
 
 }
 
